cec9c9fddcb1ef9cfc51b1e68521b411285c3f1c
KARL NASSAR <kgnassar01@gmail.com>
2021-02-08 15:43:12 +0200

    first card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './App.css';
+import mountain from './images/image.svg'
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's Start</h1>
+      <div className="app__card">
+        <h1>Upload your image</h1>
+        <p className="app__cardSub">File should be Jpeg, Png,...</p>
+        <div className="app__uploader">
+          <img src={mountain} alt="illustration"/>
+          <p>Drag & Drop your image here</p>
+        </div>
+        <p className="app__cardOr">Or</p>
+        <button>Choose a file</button>
+      </div>
     </div>
   );
 }
